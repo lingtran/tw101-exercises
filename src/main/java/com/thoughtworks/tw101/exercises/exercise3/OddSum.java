d8c2526a14bf881b0638cb56880b8e3573d72fef
lingtran <lxiutran@gmail.com>
2016-12-09 01:16:40 -0600

    Initial commit for exercise 3

--- src/main/java/com/thoughtworks/tw101/exercises/exercise3/OddSum.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise3/OddSum.java
@@ -1,8 +1,22 @@
 package com.thoughtworks.tw101.exercises.exercise3;
 
 public class OddSum {
+    private Integer sum;
+
+    private void setSum(int i) {
+        sum += i;
+    }
+
+    private int getSum() {
+        return sum;
+    }
 
     public int of(int start, int end) {
-        return -1;
+        for (int i = start; i < end; i++) {
+            if (i % 2 != 0) {
+                setSum(i);
+            }
+        }
+        return getSum();
     }
 }
