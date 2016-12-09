e8ea6d0187ce7b1f8efe86cee41e2c012875701c
lingtran <lxiutran@gmail.com>
2016-12-09 02:38:01 -0600

    Refactor implementation of exercise 3
    Rename variables to be more explicit
    Extract addition portion to own method

--- src/main/java/com/thoughtworks/tw101/exercises/exercise3/OddSum.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise3/OddSum.java
@@ -1,22 +1,26 @@
 package com.thoughtworks.tw101.exercises.exercise3;
 
 public class OddSum {
-    private Integer sum;
+    private int sum;
 
-    private void setSum(int i) {
-        sum += i;
+    private void setSum(int newValue) {
+        this.sum += newValue;
     }
 
     private int getSum() {
         return sum;
     }
 
-    public int of(int start, int end) {
+    private void calculateSumOf(int start, int end) {
         for (int i = start; i < end; i++) {
             if (i % 2 != 0) {
                 setSum(i);
             }
         }
+    }
+
+    public int of(int start, int end) {
+        calculateSumOf(start, end);
         return getSum();
     }
 }
