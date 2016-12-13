4e7933e1175814fdb976d460fe32aca42a0d259e
lingtran <lxiutran@gmail.com>
2016-12-13 17:27:50 -0600

    Implement averageArea method in RectangleAverager class
    Create initial implementation code for averageArea method

--- src/main/java/com/thoughtworks/tw101/exercises/exercise5/RectangleAverager.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise5/RectangleAverager.java
@@ -3,6 +3,12 @@ package com.thoughtworks.tw101.exercises.exercise5;
 public class RectangleAverager {
 
     public float averageArea(Rectangle[] rectangles) {
-        return 0;
+        float sumOfRectangleAreas = 0;
+
+        for(Rectangle rectangle: rectangles) {
+           sumOfRectangleAreas += rectangle.area();
+        }
+
+        return sumOfRectangleAreas/rectangles.length;
     }
 }
