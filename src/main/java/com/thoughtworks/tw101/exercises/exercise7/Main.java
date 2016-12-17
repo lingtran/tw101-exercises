ca174085d6d01ff8ef584cb6e30f23730cfd4ffd
lingtran <lxiutran@gmail.com>
2016-12-16 18:11:37 -0600

    Initial implementation of guess number program
    First implementation of guess number program with GameModerator and Randomizer classes alongside Main
    Considering a Player class

--- src/main/java/com/thoughtworks/tw101/exercises/exercise7/Main.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise7/Main.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.tw101.exercises.exercise7;
 
+import java.util.Scanner;
 // Write a program that chooses a random number between 1 and 100. Then asks the user to enter a guess. If they guess
 // right then tell them they win, otherwise tell them if they guessed too high or low. Keep asking the user to guess
 // until they get the right answer. Use classes to separate the different concerns of this program.
@@ -7,13 +8,18 @@ package com.thoughtworks.tw101.exercises.exercise7;
 public class Main {
 
     public static void main(String[] args) {
-        // program sets random number between 1 and 100
-        // ask user to enter a guess
-        // capture number that user guessed
-        // check if number is too high or too low
-        // if number is too high or too low, report that to user and ask the user to enter another guess
-        // repeat steps between user entering a guess and checking if number is high/low
-        // if the user's guess is correct, provide congratulations
+        GameModerator moderator = new GameModerator();
+        moderator.setRandomizedNumber();
+        moderator.welcomesPlayer();
+
+        Scanner player = new Scanner(System.in);
+
+        while(moderator.maintainGameIsInProgress()) {
+            Integer guessedNumber = Integer.parseInt(player.next());
+            moderator.checkPlayerGuess(guessedNumber);
+        }
+
+        player.close();
 
     }
 }
